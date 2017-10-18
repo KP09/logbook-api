3856356ffd7a6911463affcb0f86e877aae04fb9
Kees Postma <kk.postma@gmail.com>
2017-10-18 13:59:59 +0100

    Implements a Create feature for passages

--- app/controllers/api/v1/passages_controller.rb
+++ app/controllers/api/v1/passages_controller.rb
@@ -1,8 +1,21 @@
 module Api::V1
   class PassagesController < ApplicationController
     def index
-      @passages = Passage.all
+      @passages = Passage.order("created_at DESC")
       render json: @passages
+    end
+
+    def create
+      @passage = Passage.create!(passage_params)
+      render json: @passage
+    end
+
+    private
+
+    def passage_params
+      params.require(:passage).permit(:departure_port,
+        :arrival_port, :departure_date, :arrival_date, :description, :miles,
+        :hours, :night_hours, :role, :overnight, :tidal, :ocean_passage)
     end
   end
 end
